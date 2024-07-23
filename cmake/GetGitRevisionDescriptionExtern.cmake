1bd781c317282dc7c16a5e18ce60ca103fb9cff2
Rui Zhu <jerrypiglet@gmail.com>
2024-07-23 16:33:22 -0700

    added manual version to cmake/GetGitRevisionDescriptionExtern.cmake

--- cmake/GetGitRevisionDescriptionExtern.cmake
+++ cmake/GetGitRevisionDescriptionExtern.cmake
@@ -2,8 +2,10 @@
 include(cmake/GetGitRevisionDescription.cmake)
 git_describe(ApproxMVBB_VERSION "--tags" "--abbrev=0")
 
+
 if( NOT ApproxMVBB_VERSION )
-message(FATAL_ERROR "ApproxMVBB library version could not be determined!, ${ApproxMVBB_VERSION}")
+set(ApproxMVBB_VERSION 2.1.3)
+# message(FATAL_ERROR "ApproxMVBB library version could not be determined!, ${ApproxMVBB_VERSION}")
 endif()
 
 message(STATUS "${ApproxMVBB_VERSION}")
